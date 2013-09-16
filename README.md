7bdeefa7f51a7d5fc18e9f6aa052b02a39178210
JohnD-oh <ds22ds@openmailbox.org>
2013-09-16 12:10:48 -0400

    Shell for group project

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
-first-project
+Example of a Group Project
 =============
 
 > An initial demonstration of a group github project
 
-A sample R project to demonstrate collaborative work.
+* First, look at `r_src/functions_for_project.r` 
+  * Example of breaking up a script into components for collaboration
+* Then open `text_files/everyone.txt`
+  * Everyone try editing at once
+
+Remember to PULL _before_ you PUSH...
+
---
+++ r_src/functions_for_project.r
@@ -0,0 +1,22 @@
+#
+# Here is a shell of the functions we need for the upcoming project
+#
+
+add_two <- function (x,y){
+  # adds two numbers
+  # returns the sum 
+  # Tom, can you work on this?
+}
+
+mult_two <- function(x, y) {
+  # multiplies two numbers
+  # returns the product
+  # Zu, can you work on this?
+}
+
+plot_func <- function(df){
+  # This should produce density plots for the continuous
+  # variables in the dataframe, and histograms for the factor variables
+  # and text variables
+  # Who wants to handle this one?
+}
